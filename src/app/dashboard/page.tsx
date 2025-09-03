95ce49001a699c91d89ebbcd191080d37a50695e
dogpatch626 <djpepin1299@gmail.com>
2025-09-03 19:59:57 -0400

    reset space

--- src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import Form from './DevForm';
-
-export default function Page() {
-    return (
-        <div>
-            <Form />
-        </div>
-    );
-}
--- src/app/embeds.js
+++
@@ -1,3 +0,0 @@
-export default embeds = [
-    `<iframe width="560" height="315" src="https://www.youtube.com/embed/0cDe2pK4SoU?si=Wj_zYXZ5qdB7rX-B" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
-];
